74602a3a158758514f8b8ab646bde4fcee5371e1
Samirsalekar <162707298+Samirsalekar@users.noreply.github.com>
2024-06-25 19:22:51 +0530

    initial commit

---
+++ email_slicer.py
@@ -0,0 +1,23 @@
+#seperate data type within list
+# animal = ['samir',1,2,3,'remote','key',True,False,'hello',45,3]
+# string_value = [obj for obj in animal if isinstance(obj,str)]
+# int_value = [obj for obj in animal if isinstance(obj, int)]
+# boolen_value = [obj for obj in animal if isinstance(obj,bool)]
+# print(string_value)
+# print(int_value)
+# print(boolen_value)
+#slice email
+
+# email = '  samir.salekar@gmail.com  '
+# user_name = email[:email.index('@')].strip()
+# domain_name = email[email.index('@')+1:]
+# result = (f"your user name is'{user_name}' and domain name is '{domain_name}")
+# print(result)
+
+
+
+
+# list1 = [1,2,3,44,6,6,7,8,9]
+# print(list1[2:-1])
+
+
